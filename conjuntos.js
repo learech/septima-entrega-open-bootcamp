c5ae022f792eb78c618d0ec74d89635ea95c2662
Lea Rech <learech@hotmail.com>
2023-01-28 20:41:59 -0300

    subiendo

---
+++ conjuntos.js
@@ -0,0 +1,21 @@
+const familia = [
+  "Leandro Rech",  
+  "Alberto Rech",
+  "Adriana Saggini",
+  "Sebastián Rech",
+  "Abril Rech",
+  "Eva Rech",
+  "Guadalupe Olid",
+  "Ana Belén Paolantonio",
+];
+
+const miSet = new Set (familia)
+
+miSet.add("Leandro Rech")
+
+miSet.add("Javascript")
+
+
+
+
+
